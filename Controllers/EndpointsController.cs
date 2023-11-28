61fbd9a8e5379a43aeb0170d7ca9ff7fa0e8f9b5
BlueSky0322 <ryanng8628@gmail.com>
2023-11-28 15:32:23 +0800

    feat: create endpoint controller

---
+++ Controllers/EndpointsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RHM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EndpointsController : ControllerBase
+    {
+        private readonly ILogger<EndpointsController> _logger;
+
+        public EndpointsController(ILogger<EndpointsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
