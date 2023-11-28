0d86ab51f59d6b3a6533ebf05cb64ac7721d949b
BlueSky0322 <ryanng8628@gmail.com>
2023-11-29 04:19:49 +0800

    feat: create load testing scenarios for Endpoint #2

---
+++ LoadTesting/load_test.js
@@ -0,0 +1,18 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    // Key configurations for avg load test in this section
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    stages: [
+        { duration: '5m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
+        { duration: '10m', target: 100 }, // stay at 100 users for 30 minutes
+        { duration: '5m', target: 0 }, // ramp-down to 0 users
+    ],
+};
+
+export default () => {
+    const urlRes = http.get('https://localhost:7057/Hasher/ValidateHash');
+    sleep(1);
+};
---
+++ LoadTesting/smoke_test.js
@@ -0,0 +1,14 @@
+﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    vus: 3, // Key for Smoke test. Keep it at 2, 3, max 5 VUs
+    duration: '1m', // This can be shorter or just a few iterations
+};
+
+export default () => {
+    const urlRes = http.get('https://localhost:7057/Hasher/ValidateHash');
+    sleep(1);
+};
