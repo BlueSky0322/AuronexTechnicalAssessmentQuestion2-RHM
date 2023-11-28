055e7c702b5c373ccbc62d1cc010b5c2b3036043
BlueSky0322 <ryanng8628@gmail.com>
2023-11-28 15:44:24 +0800

    feat: create helper method to generate random string

--- Controllers/EndpointsController.cs
+++ Controllers/EndpointsController.cs
@@ -1,17 +1,40 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace RHM.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("Hasher")]
     [ApiController]
     public class EndpointsController : ControllerBase
     {
+        private static Random random = new Random();
+
         private readonly ILogger<EndpointsController> _logger;
 
         public EndpointsController(ILogger<EndpointsController> logger)
         {
             _logger = logger;
         }
+
+        //Endpoint 1
+        [HttpGet("GenerateNewHash")]
+        public async Task<ActionResult<string>> GenerateHash()
+        {
+            await Task.Delay(100);
+
+            return Ok();
+        }
+
+        //Generate a random string
+        private string GenerateRandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, 5) //arbitrary length of characters
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        //Hash the random string into random string hash using SHA-256
+
     }
 }
